83841c66a5d8d303490d4405c09fdb323670149c
Yamanoya <89043601510@mail.ru>
2024-02-14 13:23:12 +0300

    parsing

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import requests
+import json
 
-response = requests.get("https://playground.learnqa.ru/api/get_text")
-print(response.text)
+json_text = ('{"messages":['
+             '{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},'
+             '{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
+             )
+
+parsing_json = json.loads(json_text)
+
+print(parsing_json['messages'][1])
