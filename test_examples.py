4662ff58f733626304896d53ca7d09c80b3d3980
Yamanoya <89043601510@mail.ru>
2024-02-24 03:12:42 +0300

    exams twelve

--- test_examples.py
+++ test_examples.py
@@ -10,3 +10,9 @@ class TestExample:
         response = requests.get(url=url).cookies
         assert 'HomeWork' in response
         assert response['HomeWork'] == 'hw_value'
+
+    def test_exam_twelve_headers(self):
+        url = 'https://playground.learnqa.ru/api/homework_header'
+        response = requests.get(url=url).headers
+        assert 'x-secret-homework-header' in response
+        assert response['x-secret-homework-header'] == 'Some secret value'
