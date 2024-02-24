813079ace3859be25b92691e7a3ba81b07cce197
Yamanoya <89043601510@mail.ru>
2024-02-24 03:08:48 +0300

    exams eleven

--- test_examples.py
+++ test_examples.py
@@ -1,4 +1,12 @@
+import requests
+
 class TestExample:
     def test_exam_ten_short_phrase(self):
         phrase = input()
         assert len(phrase) < 15
+
+    def test_exam_eleven_cookie(self):
+        url = 'https://playground.learnqa.ru/api/homework_cookie'
+        response = requests.get(url=url).cookies
+        assert 'HomeWork' in response
+        assert response['HomeWork'] == 'hw_value'
