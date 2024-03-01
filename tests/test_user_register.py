40d1e5159976afdfb635e2c39cad9d84729a9a31
Yamanoya <89043601510@mail.ru>
2024-03-02 00:25:40 +0300

    ex 15

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -3,19 +3,18 @@ import requests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 
-
 class TestUserRegister(BaseCase):
 
     @pytest.mark.parametrize(
-        'email, password, username, firstname, lastname',
+        'email, password, username, firstname, lastname, error',
         [
-            ("vinkotov@example.com", "123", "learnqa", "learnqa", "learnqa"),
-            ("vinkotovexample.com", "123", "learnqa", "learnqa", "learnqa"),
-            ("vinkotov@example.com", "123", "", "learnqa", "learnqa"),
-            ("vinkotov@example.com", "123", "learnqa", "", "learnqa"),
-            ("vinkotov@example.com", "123", "learnqa", "learnqa", ""),
-            ("vinkotov@example.com", "123", "l", "learnqa", "learnqa"),
-            ("vinkotov@example.com", "123", f"{'learnqa' * 50}", "learnqa", "learnqa"),
+            ("vinkotov@example.com", "123", "learnqa", "learnqa", "learnqa", "Users with email 'vinkotov@example.com' already exists"),
+            ("vinkotovexample.com", "123", "learnqa", "learnqa", "learnqa", "Invalid email format"),
+            ("vinkotov@example.com", "123", "", "learnqa", "learnqa", "The value of 'username' field is too short"),
+            ("vinkotov@example.com", "123", "learnqa", "", "learnqa", "The value of 'firstname' field is too short"),
+            ("vinkotov@example.com", "123", "learnqa", "learnqa", "", "The value of 'lastname' field is too short"),
+            ("vinkotov@example.com", "123", "l", "learnqa", "learnqa", "The value of 'username' field is too short"),
+            ("vinkotov@example.com", "123", f"{'learnqa' * 50}", "learnqa", "learnqa", "The value of 'username' field is too long"),
         ],
         ids=['no_changes',
              'email_without_@',
@@ -23,20 +22,19 @@ class TestUserRegister(BaseCase):
              'without_firstname',
              'without_lastname',
              'one_character_username',
-             'character_more_250'
+             'character_more_250',
              ]
     )
-    def test_create_user_with_existing_email(self, email, password, username, firstname, lastname):
+    def test_create_user_with_existing_email(self, email, password, username, firstname, lastname, error):
         data = {
             'password': password,
             'username': username,
-            'firstname': firstname,
-            'lastname': lastname,
+            'firstName': firstname,
+            'lastName': lastname,
             'email': email,
         }
         response = requests.post('https://playground.learnqa.ru/api/user/', data=data)
 
         assert response.status_code == 400, f'Unexpceted status code {response.status_code}'
-        assert response.content.decode(
-            "utf-8") == f"The following required params are missed: firstName, lastName", \
-            f"Unexpected response content {response.content}"
+        assert response.content.decode("utf-8") == error
+
